00417bfc213a2af048fa5b27e2d2c14511923b3a
Bars Margetsch <45538919+Xerren09@users.noreply.github.com>
2025-01-23 02:39:36 +0200

    Update PriceSynchroniser to reload the store when prices are synced

--- PriceSynchroniser.cs
+++ PriceSynchroniser.cs
@@ -20,12 +20,8 @@ namespace ContentWarningShop
             SyncPrices();
         }
 
-        internal static void SyncPrices()
+        private static void SyncPrices()
         {
-            if (SteamLobbyMetadataHandler.InLobby == false)
-            {
-                return;
-            }
             foreach (var item in Shop._items)
             {
                 SyncPrice(item);
@@ -34,15 +30,36 @@ namespace ContentWarningShop
 
         internal static void SyncPrice(Item item)
         {
-            var key = $"__{item.PersistentID}_price";
+            if (SteamLobbyMetadataHandler.InLobby == false)
+            {
+                return;
+            }
+            var key = $"{ShopApiPlugin.MOD_GUID}_item_{item.PersistentID}_price";
+            var changed = false;
             if (SteamLobbyMetadataHandler.IsHost)
             {
                 SteamMatchmaking.SetLobbyData(SteamLobbyMetadataHandler.CurrentLobby, key, $"{item.price}");
+                changed = true;
             }
             else
             {
                 var strVal = SteamMatchmaking.GetLobbyData(SteamLobbyMetadataHandler.CurrentLobby, key);
-                item.price = int.Parse(strVal);
+                var val = int.Parse(strVal);
+                changed = item.price != val;
+                item.price = val;
+            }
+            if (changed && ShopHandler.Instance != null)
+            {
+                if (ShopHandler.Instance.m_CategoryItemDic.ContainsKey(item.Category) == false)
+                {
+                    return;
+                }
+                var idx = ShopHandler.Instance.m_CategoryItemDic[item.Category].FindIndex(shopItem => shopItem.Item == item);
+                var newItem = new ShopItem(item);
+                ShopHandler.Instance.m_CategoryItemDic[item.Category][idx] = newItem;
+                ShopHandler.Instance.m_ItemsForSaleDictionary[item.id] = newItem;
+                // Does not actually call the RPC, this is the local effect
+                ShopHandler.Instance.RPCA_ClearCart();
             }
             Debug.Log($"Item price synchronised: {item.name} ({key}) = {item.price}");
         }
