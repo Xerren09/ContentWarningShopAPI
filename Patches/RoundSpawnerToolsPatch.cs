147c06c192443cbde2abe962dadeabb1201cb60a
Bars Margetsch <45538919+Xerren09@users.noreply.github.com>
2025-02-06 23:32:11 +0200

    Target RoundSpawnerTools instead of RoundArtifactSpawner

---
+++ Patches/RoundSpawnerToolsPatch.cs
@@ -0,0 +1,19 @@
+﻿using ContentWarningShop;
+using HarmonyLib;
+using UnityEngine;
+
+namespace ShopAPI.Patches
+{
+    [HarmonyPatch(typeof(RoundSpawnerTools))]
+    internal class RoundSpawnerToolsPatch
+    {
+        [HarmonyPatch(nameof(RoundSpawnerTools.SpawnRound))]
+        [HarmonyPrefix]
+        static void SpawnRound(RoundSpawnerTools __instance)
+        {
+            var customSpawnables = Shop.CustomItems.Where(item => item.spawnable && item.itemType == Item.ItemType.Tool);
+            Debug.Log($"Added {customSpawnables.Count()} custom items marked as spawnable to {nameof(RoundSpawnerTools)}");
+            __instance.possibleSpawns.AddRange(customSpawnables);
+        }
+    }
+}
