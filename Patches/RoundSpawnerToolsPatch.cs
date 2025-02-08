1092e688d0a7691923bda6d557400f9d2bff20f7
Bars Margetsch <45538919+Xerren09@users.noreply.github.com>
2025-02-08 18:27:04 +0200

    Patch Populate instead of SpawnRound in RoundSpawnerTools

--- Patches/RoundSpawnerToolsPatch.cs
+++ Patches/RoundSpawnerToolsPatch.cs
@@ -7,9 +7,9 @@ namespace ShopAPI.Patches
     [HarmonyPatch(typeof(RoundSpawnerTools))]
     internal class RoundSpawnerToolsPatch
     {
-        [HarmonyPatch(nameof(RoundSpawnerTools.SpawnRound))]
-        [HarmonyPrefix]
-        static void SpawnRound(RoundSpawnerTools __instance)
+        [HarmonyPatch(nameof(RoundSpawnerTools.Populate))]
+        [HarmonyPostfix]
+        static void Populate(RoundSpawnerTools __instance)
         {
             var customSpawnables = Shop.CustomItems.Where(item => item.spawnable && item.itemType == Item.ItemType.Tool);
             Debug.Log($"Added {customSpawnables.Count()} custom items marked as spawnable to {nameof(RoundSpawnerTools)}");
